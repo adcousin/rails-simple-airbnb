658006a692b0957cd20e41ed2587794fd9343616
adrien cousin <adrien.cousin@lilo.org>
2020-11-18 16:38:38 +0100

    Seeded with 20 Star Wars flats

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Initializing seed...'
+
+10.times do
+  Flat.create!(
+    name: Faker::Movies::StarWars.wookiee_sentence ,
+    address: Faker::Movies::StarWars.planet,
+    description: Faker::Movies::StarWars.quote ,
+    price_per_night: Random.new.rand(1000),
+    number_of_guests: Random.new.rand(12)
+)
+end
+
+puts '============ Seeded ! ============'
